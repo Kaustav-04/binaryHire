752bfaedb073817e85c8173c81dffd47dfc87253
Soham Chakraborty <schakraborty8455@gmail.com>
2023-02-04 14:44:35 +0530

    Basic backend Done

--- engine.js
+++ engine.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
-
+app.use(cors())
 const homepage= require('./routes/home')
 app.use(homepage)
 
