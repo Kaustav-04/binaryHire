609a3c820fcb2712927dcef279e550cc9564c18a
Soham Chakraborty <schakraborty8455@gmail.com>
2023-02-05 02:10:48 +0530

    Updated

--- engine.js
+++ engine.js
@@ -25,6 +25,7 @@ app.use(clientauth)
 const { PSAV1Api, DataV1Api } = require('zeeve-zdfs-sdk');
 const dataApi = new DataV1Api('https://app.zeeve.io/zdfs-api', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGZjNWQ2MjE5ZWY3ODdjODcwZmYxOTE3MDA4NTdmMTMxNTkyNzU5MDU3N2EzMyIsImFjY291bnRfaWQiOiJjNjA4MTY1NS1iMjY4LTRiYzItOWNjYi0zYWNhMTNjOTZkMDkiLCJhY2Nlc3Nfa2V5IjoiNjVkZmM1ZDYyMTllZjc4N2M4NzBmZjE5MTcwMDg1N2YxMzE1OTI3NTkwNTc3YTMzIiwiZW1haWwiOiJzY2hha3JhYm9ydHk4NDU1QGdtYWlsLmNvbSIsImlhdCI6MTY3NTUxMzYzMSwiZXhwIjoxOTkxMDg5NjMxfQ.twmvPDVxEfPyRYCRwZ1f5P_Msn5lSOes4n13J4Sa7C0');
 app.post("/uploadzdfs", (req, res) => {
+  
   const url1= req.body.url1
   const name= req.body.name
   const hash= dataApi.uploadFile(
